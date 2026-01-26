a0f2e81fa54ecc2c33adb1f7e36bc90fd533188c
Konain Hassan <konainhassan604@gmail.com>
2026-01-27 01:55:33 +0500

    News-Website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+import NewsBoard from "./components/NewsBoard";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <NewsBoard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h1>
+      <div>
+        {articles.map((news,index) => {
+          return <NewsItem  key={index}  title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewsBoard;
